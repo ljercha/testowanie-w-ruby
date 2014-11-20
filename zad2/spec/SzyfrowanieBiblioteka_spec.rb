ee714bcc3305a99c4d32342b63e7b65f1bdd86fe
ljercha <ljercha@sigma.ug.edu.pl>
2014-11-20 10:38:39 +0100

    simplecov usuniety

--- zad2/spec/SzyfrowanieBiblioteka_spec.rb
+++ zad2/spec/SzyfrowanieBiblioteka_spec.rb
@@ -1,5 +1,3 @@
-require 'simplecov'
-SimpleCov.start
 
 require_relative '../szyfrowanie/szyfrowanieBiblioteka/'
 
--- zad3/spec/unit/kupon_spec.rb
+++ zad3/spec/unit/kupon_spec.rb
@@ -1,7 +1,4 @@
 
-require 'simplecov'
-SimpleCov.start
-
 require_relative '../../lib/kupon'
 
 
